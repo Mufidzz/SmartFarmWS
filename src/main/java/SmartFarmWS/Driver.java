df7d3e51d460fd98825b8244d9d5352504bb2c2a
Mufidzz <dmzmzzz@users.noreply.github.com>
2019-06-06 08:59:07 +0700

    Base API Finished

--- src/main/java/SmartFarmWS/Driver.java
+++ src/main/java/SmartFarmWS/Driver.java
@@ -1,4 +1,48 @@
 package SmartFarmWS;
 
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Driver {
+    public static Properties proops;
+
+    public static void main(String[] args) {
+        proops = new Properties();
+
+        if (args.length > 0) {
+            proopsLoad(proops,args[0]);
+        } else {
+            System.out.println("Main Param is Empty");
+            System.exit(0);
+        }
+
+        int port = Integer.parseInt(proops.getProperty("server.port","1234"));
+        int maxThreads = Integer.parseInt(proops.getProperty("server.maxthreads","200"));
+        int minThreads = Integer.parseInt(proops.getProperty("server.minthreads","30"));
+        int timeOut = Integer.parseInt(proops.getProperty("server.timeout","20000"));
+
+        new Server(port,minThreads,maxThreads,timeOut);
+
+    }
+    
+    public static void proopsLoad(Properties proop, String filename){
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(filename);
+            proop.load(input);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (input != null){
+                try {
+                    input.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- src/main/java/SmartFarmWS/Server.java
+++ src/main/java/SmartFarmWS/Server.java
@@ -1,4 +1,21 @@
 package SmartFarmWS;
 
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
 public class Server {
+    public Server(int port, int minThreads, int maxThreads, int timeOut){
+        Spark.port(port);
+        Spark.threadPool(maxThreads, minThreads, timeOut);
+
+        Spark.get("/", (req,res) -> "Coba");
+        apply();
+
+    }
+
+    public final static void apply(){
+        Filter f = (request, response) -> response.type("application/json");
+    }
 }
